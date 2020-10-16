ed5eaf647447cdc3026c3e59b4f1784706839f58
Meng Li <34143965+Meng6@users.noreply.github.com>
2020-10-16 07:59:57 -0400

    Exclude days inpatient because of readmission

--- src/models/select_days_to_analyse.py
+++ src/models/select_days_to_analyse.py
@@ -2,6 +2,15 @@ import numpy as np
 import pandas as pd
 from datetime import timedelta
 
+def excludeDaysInRange(days_to_analyse, start_date, end_date):
+    num_of_days = (end_date - start_date).days
+    if num_of_days > 0:
+        exclude_dates = []
+        for day in range(num_of_days + 1):
+            exclude_dates.append(start_date + timedelta(days = day))
+        days_to_analyse = days_to_analyse[~days_to_analyse["local_date"].isin(exclude_dates)]
+    return days_to_analyse
+
 def appendDaysInRange(days_to_analyse, start_date, end_date, day_type):
     num_of_days = (end_date - start_date).days
     if np.isnan(num_of_days):
@@ -23,7 +32,7 @@ def appendDaysInRange(days_to_analyse, start_date, end_date, day_type):
 days_before_surgery = int(snakemake.params["days_before_surgery"])
 days_in_hospital = str(snakemake.params["days_in_hospital"])
 days_after_discharge = int(snakemake.params["days_after_discharge"])
-participant_info = pd.read_csv(snakemake.input["participant_info"], parse_dates=["surgery_date", "discharge_date"])
+participant_info = pd.read_csv(snakemake.input["participant_info"], parse_dates=["surgery_date", "discharge_date", "readmission1_date", "readmission1_discharge", "readmission2_date", "readmission2_discharge", "readmission3_date", "readmission3_discharge"])
 days_to_analyse = pd.DataFrame(columns = ["local_date", "day_idx"])
 
 try:
@@ -39,5 +48,27 @@ else:
     if days_in_hospital == "T":
         days_to_analyse = appendDaysInRange(days_to_analyse, surgery_date, discharge_date, 0)
     days_to_analyse = appendDaysInRange(days_to_analyse, discharge_date + timedelta(days = 1), end_date, 1)
+    
+    # exclude readmission
+    try:
+        readmission1_date, readmission1_discharge = participant_info["readmission1_date"].iloc[0].date(), participant_info["readmission1_discharge"].iloc[0].date()
+    except:
+        pass
+    else:
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission1_date, readmission1_discharge)
+    
+    try:
+        readmission2_date, readmission2_discharge = participant_info["readmission2_date"].iloc[0].date(), participant_info["readmission2_discharge"].iloc[0].date()
+    except:
+        pass
+    else:
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission2_date, readmission2_discharge)
+    
+    try:
+        readmission3_date, readmission3_discharge = participant_info["readmission3_date"].iloc[0].date(), participant_info["readmission3_discharge"].iloc[0].date()
+    except:
+        pass
+    else:
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission3_date, readmission3_discharge)
     
 days_to_analyse.to_csv(snakemake.output[0], index=False)

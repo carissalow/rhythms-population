bb5f8bd76bf4ecda07fd94715f81b8ba376f23cf
Meng Li <34143965+Meng6@users.noreply.github.com>
2020-10-21 23:08:36 -0400

    add "is_readmitted" feature

--- src/models/select_days_to_analyse.py
+++ src/models/select_days_to_analyse.py
@@ -2,13 +2,20 @@ import numpy as np
 import pandas as pd
 from datetime import timedelta
 
-def excludeDaysInRange(days_to_analyse, start_date, end_date):
+def excludeDaysInRange(days_to_analyse, start_date, end_date, last_analysis_date):
     num_of_days = (end_date - start_date).days
     if num_of_days > 0:
         exclude_dates = []
         for day in range(num_of_days + 1):
             exclude_dates.append(start_date + timedelta(days = day))
         days_to_analyse = days_to_analyse[~days_to_analyse["local_date"].isin(exclude_dates)]
+        
+        num_of_last_part_days = (last_analysis_date - end_date).days
+        if num_of_last_part_days > 0:
+            for day in range(num_of_last_part_days):
+                curr_date = end_date + timedelta(days = day + 1)
+                days_to_analyse.loc[days_to_analyse["local_date"] == curr_date, "is_readmitted"] = 1
+
     return days_to_analyse
 
 def appendDaysInRange(days_to_analyse, start_date, end_date, day_type):
@@ -25,7 +32,7 @@ def appendDaysInRange(days_to_analyse, start_date, end_date, day_type):
         else:
             day_idx = 0
 
-        days_to_analyse = days_to_analyse.append({"local_date": start_date + timedelta(days = day), "day_idx": day_idx}, ignore_index=True)
+        days_to_analyse = days_to_analyse.append({"local_date": start_date + timedelta(days = day), "day_idx": day_idx, "is_readmitted": 0}, ignore_index=True)
     
     return days_to_analyse
 
@@ -33,7 +40,7 @@ days_before_surgery = int(snakemake.params["days_before_surgery"])
 days_in_hospital = str(snakemake.params["days_in_hospital"])
 days_after_discharge = int(snakemake.params["days_after_discharge"])
 participant_info = pd.read_csv(snakemake.input["participant_info"], parse_dates=["surgery_date", "discharge_date", "readmission1_date", "readmission1_discharge", "readmission2_date", "readmission2_discharge", "readmission3_date", "readmission3_discharge"])
-days_to_analyse = pd.DataFrame(columns = ["local_date", "day_idx"])
+days_to_analyse = pd.DataFrame(columns = ["local_date", "day_idx", "is_readmitted"])
 
 try:
     surgery_date, discharge_date = participant_info["surgery_date"].iloc[0].date(), participant_info["discharge_date"].iloc[0].date()
@@ -55,20 +62,20 @@ else:
     except:
         pass
     else:
-        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission1_date, readmission1_discharge)
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission1_date, readmission1_discharge, end_date)
     
     try:
         readmission2_date, readmission2_discharge = participant_info["readmission2_date"].iloc[0].date(), participant_info["readmission2_discharge"].iloc[0].date()
     except:
         pass
     else:
-        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission2_date, readmission2_discharge)
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission2_date, readmission2_discharge, end_date)
     
     try:
         readmission3_date, readmission3_discharge = participant_info["readmission3_date"].iloc[0].date(), participant_info["readmission3_discharge"].iloc[0].date()
     except:
         pass
     else:
-        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission3_date, readmission3_discharge)
+        days_to_analyse = excludeDaysInRange(days_to_analyse, readmission3_date, readmission3_discharge, end_date)
     
 days_to_analyse.to_csv(snakemake.output[0], index=False)
